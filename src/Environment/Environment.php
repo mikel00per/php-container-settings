703a8191b9d28500a2366d5bbc54fe846e3f8596
Miguel Morillo <miguelmwilliams@gmail.com>
2024-03-10 20:01:23 +0100

    improvement(env): added methods to know which is the current env

--- src/Environment/Environment.php
+++ src/Environment/Environment.php
@@ -9,4 +9,19 @@ enum Environment: string
     case DEVELOP = 'develop';
     case TESTING = 'testing';
     case PRODUCTION = 'production';
+
+    public function isDevelop(): bool
+    {
+        return $this === self::DEVELOP;
+    }
+
+    public function testing(): bool
+    {
+        return $this === self::TESTING;
+    }
+
+    public function production(): bool
+    {
+        return $this === self::PRODUCTION;
+    }
 }
